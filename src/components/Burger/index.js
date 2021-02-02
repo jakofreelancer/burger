f387baed203759fe2ec6290790e1c8056a114dd0
Javkhlan Bulgamaa <jako.freelancer@gmail.com>
2021-02-02 23:21:42 +0800

    35. burgeriin ortsiig dursleh

--- src/components/Burger/index.js
+++ src/components/Burger/index.js
@@ -2,15 +2,28 @@ import React from "react";
 import BurgerIngredient from "../BurgerIngredient";
 import css from "./style.module.css";
 
-const Burger = () => (
-    <div className={css.Burger}>
-        <BurgerIngredient type="bread-top"/>
-        <BurgerIngredient type="salad"/>
-        <BurgerIngredient type="bacon"/>
-        <BurgerIngredient type="meat"/>
-        <BurgerIngredient type="cheese"/>
-        <BurgerIngredient type="bread-bottom"/>
-    </div>
-);
+const Burger = props => {
+    //{bacon: 2, cheese: 2, meat: 1, salad: 1}
+    console.log(props.ingredients);
+    const items = Object.entries(props.ingredients);
+    console.log(items);
+    
+    let content = [];
+    items.map(el => {
+        for(let i=0; i<el[1]; i++)
+            content.push(<BurgerIngredient key={`${el[0]}${i + 1}`} type={el[0]} />);
+    });
+
+    if(content.length === 0)
+        content = <p>Хачиртай талхныхаа орцыг сонгоно уу...</p>;
+
+    return (
+        <div className={css.Burger}>
+            <BurgerIngredient type="bread-top"/>
+            {content}
+            <BurgerIngredient type="bread-bottom"/>
+        </div>
+    );
+};
 
 export default Burger;
--- src/pages/BurgerBuilder/index.js
+++ src/pages/BurgerBuilder/index.js
@@ -3,10 +3,19 @@ import "./style.css";
 import Burger from "../../components/Burger";
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            cheese: 2,
+            bacon: 2,
+            meat: 1
+        }
+    }
+
     render () {
         return (
             <div>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div>Орцны удирдлага</div>
             </div>
         );
