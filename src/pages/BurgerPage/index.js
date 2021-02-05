ecb09cb8689be09ed1a09f96440028e624a19ca9
Javkhlan Bulgamaa <jako.freelancer@gmail.com>
2021-02-05 13:52:24 +0800

    53. axios.s get ashiglan data avch haruulav

--- src/pages/BurgerPage/index.js
+++ src/pages/BurgerPage/index.js
@@ -26,7 +26,27 @@ class BurgerBuilder extends Component {
 
         totalPrice: 1000,
         purchasing: false,
-        confirmOrder: false
+        confirmOrder: false,
+        lastCustomerName: "N/A"
+    };
+
+    componentDidMount = () => {
+        axios
+            .get("/orders.json")
+            .then(response => {
+                const arr = Object.entries(response.data);
+                arr.forEach( el => {
+                    console.log(el[1].address.name + " - " + el[1].price);
+                })
+
+                const lastOrder = arr[arr.length-1][1];
+
+                this.setState({ 
+                    lastCustomerName: lastOrder.address.name, 
+                    ingredients: lastOrder.ingredient, 
+                    totalPrice: lastOrder.price 
+                });
+            })
     };
 
     showOrderConfirmModal = () => {
@@ -92,6 +112,9 @@ class BurgerBuilder extends Component {
                         ingredientNames={INGREDIENT_NAMES}
                         ingredients={this.state.ingredients} />
                 </Modal>
+                <p style={{width: "100%", textAlign: "center", fontSize: "28px" }}>
+                    Сүүлийн захиалагч: {this.state.lastCustomerName}
+                </p>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
                     showOrderConfirmModal={this.showOrderConfirmModal}
