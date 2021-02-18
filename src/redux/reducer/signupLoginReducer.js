c5e78d8389f7f9cf9a842712e73076056ab832dc
Javkhlan Bulgamaa <jako.freelancer@gmail.com>
2021-02-18 18:12:08 +0800

    91. Signup actioniig localstorage.toi holboson

--- src/redux/reducer/signupLoginReducer.js
+++ src/redux/reducer/signupLoginReducer.js
@@ -17,8 +17,8 @@ const reducer = (state = initialState, action) => {
         case "SIGNUP_USER_SUCCESS" : return {
             ...state,
             saving: false,
-            token: action.firebaseResultData.idToken,
-            userId: action.firebaseResultData.localId
+            token: action.token,
+            userId: action.userId
         };
 
 
