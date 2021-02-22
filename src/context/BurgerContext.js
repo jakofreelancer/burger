848d5df5200cdbfb66cea2ee6d488c1b221f807c
Javkhlan Bulgamaa <jako.freelancer@gmail.com>
2021-02-22 13:35:12 +0800

    105. Burger componentiig redux.aas hooks ruu

--- src/context/BurgerContext.js
+++ src/context/BurgerContext.js
@@ -1,3 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default React.createContext("Hello!");
+const BurgerContext = React.createContext();
+
+export const BurgerStore = props => {
+    const [ingredients, setIngredients] = useState({
+        salad: 0,
+        cheese: 0,
+        bacon: 0,
+        meat: 0
+    });
+
+    const addIngredient = (ingredient) => {
+        console.log("add...");
+    };
+
+    const removeIngredient = (ingredient) => {
+        console.log("remove...");
+    };
+
+    return 
+        <BurgerContext.Provider value={(ingredients, addIngredient, removeIngredient)}>
+            {props.children}
+        </BurgerContext.Provider>;
+};
+
+export default BurgerContext;
